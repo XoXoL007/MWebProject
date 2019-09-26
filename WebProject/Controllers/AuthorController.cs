071e8f57191c6873d2d300df0327aaf78954171d
User <User@WIN-BR1030AR8CO>
2019-09-26 16:23:34 +0600

    Добавьте файлы проекта.

---
+++ WebProject/Controllers/AuthorController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebProject.Controllers
+{
+    public class AuthorController : Controller
+    {
+        // GET: Author
+        public ActionResult Index()
+        {
+            List<Authors> authors;
+            using (Model1 db = new Model1())
+            {
+                authors = db.Authors.ToList();
+            }
+            return View(authors);
+        }
+
+        public ActionResult CreateAndEdit(int? id)
+        {
+            Authors author;
+            using (Model1 db = new Model1())
+            {
+                author = db.Authors.Where(a => a.Id == id).FirstOrDefault();
+                if (author != null)
+                {
+                    return View(author);
+                }
+                else
+                {
+                    return View();
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult CreateAndEdit(Authors author)
+        {
+            using (Model1 db = new Model1())
+            {
+                if (author.Id != 0)
+                {
+                    var oldAuthor = db.Authors.Where(a => a.Id == author.Id).FirstOrDefault();
+                    oldAuthor.FirstName = author.FirstName;
+                    oldAuthor.LastName = author.LastName;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index", "Author");
+                }
+                else
+                {
+                    db.Authors.Add(author);
+                    db.SaveChanges();
+                    return Redirect("Index");
+                }
+
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            using (Model1 db = new Model1())
+            {
+                var author = db.Authors.Where(a => a.Id == id).FirstOrDefault();
+                db.Authors.Remove(author);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index", "Author");
+        }
+    }
+}
---
+++ WebProject/Controllers/BookController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebProject.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            List<Books> books;
+            using (Model1 db = new Model1())
+            {
+                books = db.Books.ToList();
+            }
+            return View(books);
+        }
+
+        public ActionResult CreateAndEdit(int? id)
+        {
+            Books book;
+            using (Model1 db = new Model1())
+            {
+                book = db.Books.Where(a => a.Id == id).FirstOrDefault();
+                if (book != null)
+                {
+                    return View(book);
+                }
+                else
+                {
+                    return View();
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult CreateAndEdit(Books book)
+        {
+            using (Model1 db = new Model1())
+            {
+                if (book.Id != 0)
+                {
+                    var oldBook = db.Books.Where(a => a.Id == book.Id).FirstOrDefault();
+                    oldBook.Title = book.Title;
+                    oldBook.Price = book.Price;
+                    oldBook.Pages = book.Pages;
+                    oldBook.AuthorId = book.AuthorId;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index", "Book");
+                }
+                else
+                {
+                    db.Books.Add(book);
+                    db.SaveChanges();
+                    return Redirect("Index");
+                }
+
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            using (Model1 db = new Model1())
+            {
+                var book = db.Books.Where(a => a.Id == id).FirstOrDefault();
+                db.Books.Remove(book);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index", "Book");
+        }
+    }
+}
